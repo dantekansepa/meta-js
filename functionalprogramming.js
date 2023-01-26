9989e0a58a78379858f8d29d760aef9b77202dd1
dantekansepa <vsionpy@gmail.com>
2023-01-26 12:01:27 +0200

    created code for the consleStyler()

--- functionalprogramming.js
+++ functionalprogramming.js
@@ -1,6 +1,11 @@
 // Task 1: Build a function-based console log message generator
 function consoleStyler() {
-    
+   var message = "%c" + text;
+   var style = `color: ${color};`
+   style += `background: ${background};`
+   style += `font-size: ${fontSize};`
+
+   console.log(message, style)
 }
 
 // Task 2: Build another console log message generator
