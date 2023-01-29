f596b0a387e9b1b1d036f02daf090f8bfc4e3a27
dantekansepa <vsionpy@gmail.com>
2023-01-29 17:07:59 +0200

    added a for loop in the function

---
+++ solution.js
@@ -0,0 +1,12 @@
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yoghurt', 'ice cream', 'milkshake']
+function logDiary(arr) {
+    for (i of arr) {
+        console.log(i);
+    }
+}
+console.log(logDiary(dairy));
+// Task 2
+
+// Task 3
+
