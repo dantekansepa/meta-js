5aba19173c23bfae48d802bbbeff8870c7f4a509
dantekansepa <vsionpy@gmail.com>
2023-01-30 13:12:46 +0200

    removing the 4th task code

--- solution.js
+++ solution.js
@@ -1,12 +1,31 @@
 // Task 1
-var dairy = ['cheese', 'sour cream', 'milk', 'yoghurt', 'ice cream', 'milkshake']
+var dairy = ['cheese', 'sour cream', 'milk', 'yoghurt', 'ice cream', 'milkshake'];
 function logDiary(arr) {
     for (i of arr) {
         console.log(i);
     }
 }
 console.log(logDiary(dairy));
+console.log('\n')
 // Task 2
+const animal = {
 
+canJump: true
+
+};
+
+const bird = Object.create(animal);
+
+bird.canFly = true;
+
+bird.hasFeathers = true;
+
+function birdCan(a) {
+    for (key of Object.keys(a)) {
+        console.log(key, +':' + a[key]);
+    }
+}
+console.log(birdCan(bird));
 // Task 3
+
 
